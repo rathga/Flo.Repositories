247af888d60379428d66ea02f4e8928494dba690
Richard Davies <rathga@gmail.com>
2020-03-24 13:48:57 +0000

    Tweaked MongoDB dependency injection

--- Flo.Repositories.MongoDB/DependencyInjection.cs
+++ Flo.Repositories.MongoDB/DependencyInjection.cs
@@ -27,23 +27,20 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class ServiceCollectionExtensions
     {
 
-        public static IServiceCollection AddMongoDBCollection<T>(this IServiceCollection services, string connectionString, string databaseName, string collectionName)
+        public static IServiceCollection AddMongoDBCollection<T, TKeyType>(this IServiceCollection services, string connectionString, string databaseName, string collectionName)
+            where T : Entity<TKeyType>
         {
-            return services.AddScoped<IMongoCollection<T>>(provider => MongoHelper.GetCollection<T>(connectionString, databaseName, collectionName));
+            return services.AddScoped(provider => MongoHelper.GetCollection<T>(connectionString, databaseName, collectionName))
+                .AddScoped<ICrud<T, TKeyType>, Crud<T, TKeyType>>()
+                .AddScoped<IRawGet<T, TKeyType>, RawGet<T, TKeyType>>();
         }
 
-        public static IServiceCollection AddMongoDBRepo<T, TKeyType, TRepo, TValidator, TAuthoriser>(this IServiceCollection services, string connectionString, string databaseName, string collectionName)
+        public static IServiceCollection AddMongoDBRepo<T, TKeyType, TRepo>(this IServiceCollection services, string connectionString, string databaseName, string collectionName)
             where T : Entity<TKeyType>
             where TRepo : class, IRepository<T, TKeyType>
-            where TValidator : class, IValidator<T, TKeyType>
-            where TAuthoriser : class, IAuthoriser<T, TKeyType>
         {
-                 return services.AddMongoDBCollection<T>(connectionString, databaseName, collectionName)
-                .AddScoped<IRepository<T, TKeyType>, TRepo>()
-                .AddScoped<IValidator<T, TKeyType>, TValidator>()
-                .AddScoped<IAuthoriser<T, TKeyType>, TAuthoriser>()
-                .AddScoped<IRawGet<T, TKeyType>, RawGet<T, TKeyType>>()
-                .AddScoped<ICrud<T, TKeyType>, Crud<T, TKeyType>>();
+            return services.AddMongoDBCollection<T, TKeyType>(connectionString, databaseName, collectionName)
+           .AddScoped<IRepository<T, TKeyType>, TRepo>();
         }
     }
 }
